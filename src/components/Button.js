684b915c62a2f3af369ad3472cd42bc9f3d4af0e
scott <scottakirschner@gmail.com>
2020-05-09 21:17:27 -0400

    consuming multiple contexts

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 
 class Button extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    return <button className="ui button primary">
-        <LanguageContext.Consumer>
-            {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
-        </LanguageContext.Consumer>
-    </button>;
+    return (
+      <ColorContext.Consumer>
+        {(color) => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {(language) => (language === 'english' ? 'Submit' : 'Voorleggen')}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
+    );
   }
 }
 
