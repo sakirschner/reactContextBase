8f687b280218700809ca50b6411311526fa51fa2
scott <scottakirschner@gmail.com>
2020-05-09 21:09:51 -0400

    context consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,8 +5,11 @@ class Button extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
-    return <button className="ui button primary">{text}</button>;
+    return <button className="ui button primary">
+        <LanguageContext.Consumer>
+            {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+        </LanguageContext.Consumer>
+    </button>;
   }
 }
 
